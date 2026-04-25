0b3e78c5df9b13f0dc0f688de7cef93ee5b2fb99
dinelkagui <dinelka@guisrilanka.com>
2026-04-25 22:09:41 +0530

    add ed

---
+++ src/types/types.ts
@@ -0,0 +1,20 @@
+export interface Address {
+  line1: string;
+  line2: string;
+  cityId: number;
+  countryId: number;
+}
+
+export interface Customer {
+  id?: number;
+  name: string;
+  dob: string;
+  nic: string;
+  mobiles: string[];
+  addresses: Address[];
+}
+
+export interface Family {
+  customerId: number;
+  familyMemberId: number;
+}
